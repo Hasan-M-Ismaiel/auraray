8ac8ee32042dfde66a49690c799090f586f4c7d1
hasan <hasanismaiel19951995@gmail.com>
2024-12-22 19:39:49 +0300

    front end theme

--- resources/views/includes/products.blade.php
+++ resources/views/includes/products.blade.php
@@ -12,50 +12,62 @@
         <!--search bar-->
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="input-group mb-3">
-                        <select id="category" class="form-select" style="max-width: 150px;">
-                            <option class="option_" selected>Category</option>
-                            <option class="option_" value="Hair Care">Hair Care</option>
-                            <option class="option_" value="Shaving and Grooming">Shaving and Grooming</option>
-                            <option class="option_" value="Skin Care">Skin Care</option>
-                        </select>
-                        <select id="type" class="form-select" style="max-width: 150px;">
-                            <option class="option_" selected>Type</option>
-                            <option class="option_" value="Shampoo">Shampoo</option>
-                            <option class="option_" value="Conditioner">Conditioner</option>
-                            <option class="option_" value="Hair treatment cream">Hair treatment cream</option>
-                            <option class="option_" value="Face scrub">Face scrub</option>
-                            <option class="option_" value="Foot & Body scrub">Foot & Body scrub</option>
-                            <option class="option_" value="Shower gel">Shower gel</option>
-                        </select>
-                        <select id="flavor" class="form-select" style="max-width: 150px;">
-                            <option class="option_" selected>Flavor</option>
-                            <option class="option_" value="Aloevera">Aloevera</option>
-                            <option class="option_" value="Olive oil">Olive oil</option>
-                            <option class="option_" value="Strawberry">Strawberry</option>
-                            <option class="option_" value="Lavendar">Lavendar</option>
-                            <option class="option_" value="Coconut oil">Coconut oil</option>
-                            <option class="option_" value="Argan oil">Argan oil</option>
-                        </select>
-                        <select id="size" class="form-select" style="max-width: 150px;">
-                            <option class="option_" selected>Size</option>
-                            <option class="option_" value="500">500ml</option>
-                            <option class="option_" value="1000">1000ml</option>
-                            <option class="option_" value="1200">1200ml</option>
-                            <option class="option_" value="4500">4500ml</option>
-                            <option class="option_" value="5000">5000ml</option>
-                        </select>
-                        <button id="search" class="btn search_" type="submit">Search</button>
+                <div class="col-12 col-md-10 col-lg-8">
+                    <div class="row gx-2 gy-2 align-items-center">
+                        <div class="col-6 col-md-3">
+                            <select id="category" class="form-select theme-select">
+                                <option class="option_" selected>Category</option>
+                                <option class="option_" value="Hair Care">Hair Care</option>
+                                <option class="option_" value="Shaving and Grooming">Shaving and Grooming</option>
+                                <option class="option_" value="Skin Care">Skin Care</option>
+                            </select>
+                        </div>
+                        <div class="col-6 col-md-3">
+                            <select id="type" class="form-select theme-select">
+                                <option class="option_" selected>Type</option>
+                                <option class="option_" value="Shampoo">Shampoo</option>
+                                <option class="option_" value="Conditioner">Conditioner</option>
+                                <option class="option_" value="Hair treatment cream">Hair treatment cream</option>
+                                <option class="option_" value="Face scrub">Face scrub</option>
+                                <option class="option_" value="Foot & Body scrub">Foot & Body scrub</option>
+                                <option class="option_" value="Shower gel">Shower gel</option>
+                            </select>
+                        </div>
+                        <div class="col-6 col-md-3">
+                            <select id="flavor" class="form-select theme-select">
+                                <option class="option_" selected>Flavor</option>
+                                <option class="option_" value="Aloevera">Aloevera</option>
+                                <option class="option_" value="Olive oil">Olive oil</option>
+                                <option class="option_" value="Strawberry">Strawberry</option>
+                                <option class="option_" value="Lavendar">Lavendar</option>
+                                <option class="option_" value="Coconut oil">Coconut oil</option>
+                                <option class="option_" value="Argan oil">Argan oil</option>
+                            </select>
+                        </div>
+                        <div class="col-6 col-md-3">
+                            <select id="size" class="form-select theme-select">
+                                <option class="option_" selected>Size</option>
+                                <option class="option_" value="500ml">500ml</option>
+                                <option class="option_" value="1000ml">1000ml</option>
+                                <option class="option_" value="1200ml">1200ml</option>
+                                <option class="option_" value="4500ml">4500ml</option>
+                                <option class="option_" value="5000ml">5000ml</option>
+                            </select>
+                        </div>
+                        <div class="col-12 text-center">
+                            <button id="search" class="btn search_ theme-btn w-100" type="submit">Search</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
+
+
         <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
 
             <ul class="product-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
-                <li class="filter-active" id="reset">Reset</li>
+                <li class="filter-active mt-2" id="reset" style="color: white;">Reset</li>
             </ul><!-- End product Filters -->
 
             <div id="products-lists">
